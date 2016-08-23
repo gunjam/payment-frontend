e2950ff921c03f038796f4a38d33bd93ebca5fac
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-08-23 14:23:44 +0100

    Remove crap I'm not using

--- src/utils/is-numeric.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const isNumeric = /^[0-9]+$/;
-
-module.exports = function (input) {
-  return (typeof input === 'string') ? isNumeric.test(input) : false;
-};
--- src/utils/is-valid-nino.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const validNino = /^(?!BG|GB|NK|KN|TN|NT|ZZ)[ABCEGHJ-PRSTW-Z]{2}\d{6}[A-D]$/i;
-const allSpaces = /\s/g;
-
-module.exports = function (input) {
-  const sanitised = `${input}`.replace(allSpaces, '');
-  return validNino.test(sanitised);
-};
