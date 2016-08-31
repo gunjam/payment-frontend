da0167064011759d188244a73c2907f57edb1a83
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-08-31 16:33:04 +0100

    generateBspSchedule() is more accurate

--- src/lib/generate-bsp-schedule.js
+++ src/lib/generate-bsp-schedule.js
@@ -2,53 +2,46 @@
 
 const moment = require('moment');
 
+const maxMonthlyPayments = 17;
 const dateFormat = 'YYYY-MM-DD';
 
-module.exports = function (dateOfClaim, dateOfDeath, rateOfPayment) {
-  const initialAmount = (rateOfPayment === 'higher') ? 5000 : 2500;
-  const monthlyAmount = (rateOfPayment === 'higher') ? 500 : 100;
+module.exports = function (dateOfClaim, dateOfDeath, dateOfPensionAge, rate) {
+  const schedule = [];
+  const initialAmount = (rate === 'higher') ? 5000 : 2500;
+  const monthlyAmount = (rate === 'higher') ? 500 : 100;
 
-  const startDate = moment(dateOfClaim).add(1, 'months').set('date', dateOfDeath.getDate());
+  const payDate = moment(dateOfClaim).add(1, 'months').set('date', dateOfDeath.getDate());
+  const monthsTillPensionable = moment(dateOfPensionAge).diff(dateOfClaim, 'months');
   const monthsSinceDeath = moment(dateOfClaim).diff(dateOfDeath, 'months');
 
-  const numberOfPayments = 17 - monthsSinceDeath;
-  const schedule = [];
+  const numberOfPayments = Math.min(maxMonthlyPayments - monthsSinceDeath, monthsTillPensionable - 2);
 
   if (monthsSinceDeath < 12) {
-    schedule.push({
-      amount: initialAmount,
-      date: startDate.format(dateFormat)
-    });
-    startDate.add(1, 'months');
+    schedulePayment(schedule, initialAmount, payDate);
   }
 
-  if (monthsSinceDeath < 1) {
-    schedule.push({
-      amount: monthlyAmount,
-      date: startDate.format(dateFormat)
-    });
-  } else if (monthsSinceDeath < 2) {
-    schedule.push({
-      amount: monthlyAmount * 2,
-      date: startDate.format(dateFormat)
-    });
-  } else {
-    schedule.push({
-      amount: monthlyAmount * 3,
-      date: startDate.format(dateFormat)
-    });
+  if (monthsTillPensionable > 1) {
+    const firstMonthlyPayment = monthsSinceDeath < 1 ? monthlyAmount :
+      monthlyAmount * Math.min(monthsSinceDeath + 1, 3);
+    schedulePayment(schedule, firstMonthlyPayment, payDate);
   }
 
   for (let i = 0; i < numberOfPayments; i++) {
-    const payment = {
-      amount: monthlyAmount,
-      date: startDate.add(1, 'months').format(dateFormat)
-    };
-    schedule.push(payment);
+    schedulePayment(schedule, monthlyAmount, payDate);
   }
   return schedule;
 };
 
+function schedulePayment(schedule, amount, moment) {
+  schedule.push({
+    amount,
+    date: moment.format(dateFormat)
+  });
+  moment.add(1, 'months');
+}
+
 function monthsBetween(date1, date2) {
   return date2.getMonth() - date1.getMonth() + ((date2.getFullYear() - date1.getFullYear()) * 12);
 }
+
+// function getPayDat
