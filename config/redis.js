3e2913ef1bf6affe57c03662883bf0ddf997dfc5
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-09-19 17:45:25 +0100

    WORK

--- config/redis.js
+++ config/redis.js
@@ -1,12 +1,18 @@
 'use strict';
 
-const redis = require('url').parse(process.env.REDIS_URL || 'localhost:6379');
-
-console.log(redis);
+const url = require('url');
 
 module.exports = {
-  host: redis.hostname,
-  port: redis.port,
   prefix: 'payment-frontend',
   db: 0
 };
+
+if (process.env.REDIS_URL) {
+  const redis = url.parse(process.env.REDIS_URL);
+  module.exports.host = redis.hostname;
+  module.exports.port = redis.port;
+  module.exports.pass = redis.auth.split(':')[1];
+} else {
+  module.exports.host = 'localhost';
+  module.exports.port = 6379;
+}
