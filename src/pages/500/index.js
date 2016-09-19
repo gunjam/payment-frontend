bff57a4395d10b9712b65dbf55c2d8c8416e6b23
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-09-19 16:58:36 +0100

    Log errors from error page

--- src/pages/500/index.js
+++ src/pages/500/index.js
@@ -6,4 +6,5 @@ const developmentMode = process.env.NODE_ENV === 'development';
 
 module.exports = function (err, req, res, _next) {
   template.render({err, developmentMode}, res);
+  console.error(err);
 };
