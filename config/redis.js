d278d4c22972c822850997b8bfbaeb5ba33fbc27
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-09-19 17:17:58 +0100

    Put port back in, parse from URL

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,10 @@
+'use strict';
+
+const redis = require('url').parse('process.env.REDIS_URL' || 'localhost:6379');
+
 module.exports = {
-  host: process.env.REDIS_URL || 'localhost',
+  host: redis.hostname,
+  port: redis.port,
   prefix: 'payment-frontend',
   db: 0
 };
