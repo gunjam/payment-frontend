40b01a03ffa5b48a42879d89263470f65c935d59
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-09-19 17:24:13 +0100

    Don't do env process as a string -,-;

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const redis = require('url').parse('process.env.REDIS_URL' || 'localhost:6379');
+const redis = require('url').parse(process.env.REDIS_URL || 'localhost:6379');
+
+console.log(redis);
 
 module.exports = {
   host: redis.hostname,
