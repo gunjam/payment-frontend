ef311ee1dfadde473fce153489f5849e86c82774
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-10-04 09:20:40 +0100

    Simplify split schedule function

--- src/lib/split-schedule.js
+++ src/lib/split-schedule.js
@@ -2,6 +2,5 @@
 
 module.exports = function (schedule, dateToSplitOn = new Date()) {
   const splitIndex = schedule.findIndex(payment => new Date(payment.date) > dateToSplitOn);
-  const sliceIndex = splitIndex === -1 ? schedule.length : splitIndex;
-  return [schedule.slice(0, sliceIndex), schedule.slice(sliceIndex)];
+  return splitIndex === -1 ? [schedule, []] : [schedule.slice(0, splitIndex), schedule.slice(splitIndex)];
 };
