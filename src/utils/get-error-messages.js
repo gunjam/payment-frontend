3e11c99b33a8355d6aa0b499aaf9c5dfabe29f39
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-10-24 16:34:46 +0100

    Removed unused getErrorMessages()

--- src/utils/get-error-messages.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function (name, codes, req) {
-  return Object.keys(codes).reduce((errors, code) => {
-    errors[code] = req.t(`${name}:form.${code}.errors.${codes[code][0]}`);
-    return errors;
-  }, {});
-};
