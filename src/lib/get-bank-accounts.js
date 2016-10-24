cc64aa2e3ec33be3e964ab55155fb628ed1a6083
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-10-24 16:42:39 +0100

    Remove unused getBankAccounts()

--- src/lib/get-bank-accounts.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function (payments) {
-  return payments.slice(1).reduce(
-    (pre, cur) => pre[pre.length - 1].id === cur.bankAccount.id ? pre : [...pre, cur.bankAccount],
-    [payments[0].bankAccount]
-  );
-};
