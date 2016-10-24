8acd655cfa788cb89afa669f85d6f11127e1f1d1
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-10-24 12:01:45 +0100

    Remove numberOfBankAccounts function name

--- src/lib/number-of-bank-accounts.js
+++ src/lib/number-of-bank-accounts.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports = function numberOfBankAccounts(schedule) {
+module.exports = function (schedule) {
   return schedule.map(i => i.bankAccountId).filter((v, i, a) => i === a.lastIndexOf(v)).length;
 };
